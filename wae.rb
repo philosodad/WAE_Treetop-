8e9abecca917d2c0e0dc57425c1b6fe4094bafe3
j.paul <j.paul.daigle@gmail.com>
2009-10-24 03:56:13 -0400

    changes from bitbucket development branches. Grammar implemented.

--- wae.rb
+++ wae.rb
@@ -1,4 +1,22 @@
 require 'rubygems'
 require 'treetop'
+require 'polyglot'
 
 Treetop.load "waeg"
+
+class Runtime
+  def initialize
+    @wae = WaeParser.new
+  end
+
+  def start
+    while true
+      print 'WAE > '
+      input = gets.chomp
+      puts @wae.parse(input).eval
+    end
+  end
+
+end
+
+Runtime.new.start
